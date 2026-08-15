6797472b3e6837883e0ff3d3c1ade82699dd7513
maxwell <maxwell0developer522@gmail.com>
2026-08-15 11:59:22 +0300

    WOw

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <string>
 
+std::string text = "Hello";
 int main(){
     std::cout << "HelloWorld" << std::endl;
+    std::cout << text << std::endl;
+
     return 0;
 }
